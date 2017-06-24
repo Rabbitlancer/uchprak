d8f3966e55617ca9d423a3b3dd685230327b358b
MilanaGaiduk <milagaiduk@gmail.com>
2017-06-25 01:19:47 +0300

    Реализация ввода-вывода графа

---
+++ src/InputOutput.java
@@ -0,0 +1,71 @@
+package com.example.TwoClasses;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+//import java.util.Arrays;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class InputOutput {
+
+    private BufferedReader cin;
+    private PrintWriter cout;
+    private StringTokenizer tokenizer;
+
+    private int numV; //количество вершин в орграфе
+    private int numE; //количество дуг в орграфе
+    //список инцидентности орграфа
+    private ArrayList<Integer> Graph[];
+    private int[] component; //массив компонент сильной связности графа
+
+    int componentNum; //количество компонент связности в орграфе
+
+
+    //ввод данных с консоли
+    public void getData(ArrayList[] Graph) throws IOException {//передаем ссылку на граф, в который надо считать данные,
+        cin = new BufferedReader(new InputStreamReader(System.in));
+        cout = new PrintWriter(System.out);
+
+        tokenizer = new StringTokenizer(cin.readLine());
+        numV = Integer.parseInt(tokenizer.nextToken()); //считываем количество вершин графа
+        numE = Integer.parseInt(tokenizer.nextToken()); //считываем количество ребер графа
+
+        //инициализация списка инцидентности графа
+        Graph = new ArrayList[numV];
+        for (int i = 0; i < numV; ++i) {
+            Graph[i] = new ArrayList<Integer>();
+        }
+        //считываем граф, заданный списком ребер
+        for (int i = 0; i < numE; ++i) {
+            tokenizer = new StringTokenizer(cin.readLine());
+            int v = Integer.parseInt(tokenizer.nextToken());
+            int w = Integer.parseInt(tokenizer.nextToken());
+            v--;
+            w--;
+            Graph[v].add(w);
+        }
+    }
+
+    //вывод результата.   передаем в метод кол-во компонент связности, кол-во вершин, массив компонет сявзности - данные, которые надо вывести
+    public void printData(int componentNum,int numV,int[] component) throws IOException {
+
+        cout.println(componentNum);
+        for (int i = 0; i < numV; ++i) {
+            cout.print((component[i] + 1) + " ");
+        }
+        cout.println();
+
+        cin.close();
+        cout.close();
+    }
+    public static void main() throws IOException {
+        getData(Graph);
+        Algorithm solution = new Algorithm();
+        componentNum =  solution.run(component);
+        printData(componentNum, numV,component);
+    }
+}
+
