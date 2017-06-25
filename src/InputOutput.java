bdc687539c66979f5e2cdebbed64211227d6ccd7
MilanaGaiduk <milagaiduk@gmail.com>
2017-06-25 18:24:32 +0300

    исправление

--- src/InputOutput.java
+++ src/InputOutput.java
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
+=======
 package com.example.TwoClasses;
 
+>>>>>>> c14d5b6112b2cfcb9b9b5066daec1616a6271e92
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -31,6 +34,10 @@ public class InputOutput {
     public MyGraph getData(MyGraph Graph) throws IOException {//передаем ссылку на граф, в который надо считать данные,
         cin = new BufferedReader(new InputStreamReader(System.in));
         cout = new PrintWriter(System.out);
+<<<<<<< HEAD
+        Graph = new MyGraph();
+=======
+>>>>>>> c14d5b6112b2cfcb9b9b5066daec1616a6271e92
 
         tokenizer = new StringTokenizer(cin.readLine());
         Graph.numV = Integer.parseInt(tokenizer.nextToken()); //считываем количество вершин графа
@@ -59,7 +66,11 @@ public class InputOutput {
         cout.println(Graph.componentNum);
         for (int i = 0; i < Graph.numV; ++i) {
             //РАЗОБРАТЬСЯ, ПОЧЕМУ НЕ ВЫВОДИТ КОМПОНЕНТЫ СВЯЗНОСТИ!!! (проблема с передачей массива component)
+<<<<<<< HEAD
+            // cout.print((Graph.component[i] + 1) + " ");
+=======
          // cout.print((Graph.component[i] + 1) + " ");
+>>>>>>> c14d5b6112b2cfcb9b9b5066daec1616a6271e92
             cout.print("c");
         }
         cout.println();
