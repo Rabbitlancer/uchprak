2d610eb016984d529ea4387985c8fd232fed7fdf
MilanaGaiduk <milagaiduk@gmail.com>
2017-06-27 02:53:00 +0300

    Оконное приложение с использованием Swing, алгоритма пока нет

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,46 +1,23 @@
-import com.trolltech.qt.gui.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-import java.util.List;
-
-//объект виджета, который принимает данные визуализированного графа и выводит их на окно
-public class Canvas extends QGraphicsView {
-    private VGraph content;
-    private QGraphicsScene scene;
-    private QBrush brWhite = new QBrush(new QColor(255,255,255));
-    private QBrush brBlack = new QBrush(new QColor(0,0,0));
-    private QPen pnBlack = new QPen(new QColor(0,0,0));
-
-    public Canvas(QWidget parent) {
-        super(parent);
-        this.scene = new QGraphicsScene(0,0,428,300,this);
-        this.setScene(this.scene);
-        this.scene.setBackgroundBrush(new QBrush(new QColor(255,255,255)));
-        this.scene.setForegroundBrush(new QBrush(new QColor(255,255,255)));
-        this.ensureVisible(0,0,428,300,0,0);
-        this.pnBlack.setBrush(brBlack);
-        this.pnBlack.setWidth(2);
-        this.centerOn(214,150);
+public class Canvas extends JPanel {
+    public Canvas() {
     }
-
-    public void setContent(MyGraph data) {
-        content = new VGraph(data);
-        content.generateLayout();
-        this.redraw();
+    public void paintComponent(Graphics g) {
+        int width = getWidth();
+        int height = getHeight();
+        Color MyColor  = new Color(0,0,0);
+        g.setColor(MyColor);
+        g.drawOval(0, 0, width, height);
     }
-
-    public void redraw() {
-        this.scene.setForegroundBrush(brWhite);
-        this.scene.items().add(new QGraphicsRectItem(0,0,428,300));
-
-        List<Coord> ver = content.getCoord();
-        for (Coord a: ver) {
-            this.scene.addEllipse(a.x-10, a.y-10, 20, 20, pnBlack, brWhite);
-        }
-
-        this.setScene(this.scene);
-        this.ensureVisible(this.scene.sceneRect());
-        this.update();
-        this.repaint();
-        this.show();
+    public static void main(String args[]) {
+        JFrame frame = new JFrame("Oval Sample");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new Canvas());
+        frame.setSize(300, 200);
+        frame.setVisible(true);
     }
 }
